c8cd6a05c413b56efff753881acea8eb9a5dc6ab
Mikel <mikel.agirrebeitia@alumni.mondragon.edu>
2020-04-24 09:12:35 +0200

    Modifico CargaFicheros.R

--- CargaFicheros.R
+++ CargaFicheros.R
@@ -2,3 +2,5 @@ setwd("C:\\Users\\Mikel\\Desktop\\Ejercicio-Git")
 Confirmed<- read.csv("time_series_19-covid-Confirmed.csv")
 Death<- read.csv("time_series_19-covid-Deaths.csv")
 Recovered<- read.csv("time_series_19-covid-Recovered.csv")
+#Este es el rograma de carga de datos
+#Data <- read.csv()
